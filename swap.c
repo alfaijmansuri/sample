c355b2d994ba6b1e5555b75a664ab975ca6d06b1
goyalabhi12 <54411996+goyalabhi12@users.noreply.github.com>
2019-10-12 11:21:12 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,17 +1,21 @@
 #include <stdio.h> 
-  
+int swap(int *x,int *y)
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
 int main() 
 { 
     //hello world
+  
     int x, y; 
     printf("Enter Value of x "); 
     scanf("%d", &x); 
     printf("\nEnter Value of y "); 
     scanf("%d", &y); 
   
-    int temp = x; 
-    x = y; 
-    y = temp; 
+    swap(&x,&y);
   
     printf("\nAfter Swapping: x = %d, y = %d", x, y); 
     return 0; 
